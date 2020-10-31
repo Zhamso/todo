a4b1288f9e1013b53d731542def56b12d8a9569b
Zhamso <chimitovz3331@ya.ru>
2020-10-31 17:05:46 +0800

    first commit

---
+++ src/index.php
@@ -0,0 +1,25 @@
+<?php
+$method = $_SERVER['REQUEST_METHOD'];
+$path = $_SERVER['REQUEST_URI'];
+
+$todos = [
+	"Install Debian",
+	"(A) Read about Nginx location",
+	"(B) Fix bash complition"
+];
+
+function serialize_model(int $id, string $content) {
+	return json_encode([
+		'id'		 => $id,
+		'content'	 => $content,
+	]);
+}
+
+if($method == 'GET' && $path == '/v1/todos') {
+	http_response_code(501);
+} else if($method == 'GET' && preg_match('|^/v1/todos/(\d{1,9})$|', $path, $matches)) {
+	$id = $matches[1];
+	http_response_code(501);
+} else {
+	http_response_code(404);
+}
